3119f5cea262f0ccda399a2b62bf08fed08dd949
Ksenia <agalaksu111@gmail.com>
2017-06-18 21:48:30 +0300

    Adding new element to list

---
+++ main.js
@@ -0,0 +1,12 @@
+ document.querySelector(".myInput").addEventListener("keypress", function newLi(e) {
+  var li = document.createElement("li");
+  var inputValue = document.querySelector(".myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+
+  if(e.keyCode == 13) {
+   li.className = "newEl"
+   li.classList.add("list-group-item")
+   document.querySelector(".myUl").appendChild(li);
+  }
+});
