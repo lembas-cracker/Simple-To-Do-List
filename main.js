901df0e38908248f8fe1bf702a892aaa10f358b6
Ksenia <agalaksu111@gmail.com>
2017-06-20 01:08:23 +0300

    Remove list element

--- main.js
+++ main.js
@@ -1,17 +1,34 @@
  document.querySelector(".myInput").addEventListener("keypress", function newLi(e) {
   var li = document.createElement("li");
+
+  var button = document.createElement("span");
+  button.classList.add("glyphicon");
+  button.classList.add("glyphicon-remove-circle");
+
   var inputValue = document.querySelector(".myInput").value;
   var t = document.createTextNode(inputValue);
   li.appendChild(t);
+  li.appendChild(button)
 
-  if(e.keyCode == 13 && inputValue !=='') {
+   if(e.keyCode == 13 && inputValue !=='') {
    li.className = "newEl"
    li.classList.add("list-group-item")
    document.querySelector(".myUl").appendChild(li);
    document.querySelector(".myInput").value = '';
   }
 
+   var remove = document.querySelectorAll(".list-group-item");
+   var i;
+   for (i = 0; i < remove.length; i++) {
+    remove[i].onclick = function() {
+    var div = this.parentElement;
+    this.remove(div);
+  }
+}
+
+
 });
+
 
 
 
