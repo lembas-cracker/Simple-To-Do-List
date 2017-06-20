c5be436503fd08438081f69578b7fd7ef647cb85
Ksenia <agalaksu111@gmail.com>
2017-06-20 22:44:52 +0300

    beautify js

--- main.js
+++ main.js
@@ -1,34 +1,33 @@
  document.querySelector(".myInput").addEventListener("keypress", function newLi(e) {
-  var li = document.createElement("li");
+    var li = document.createElement("li");
 
-  var button = document.createElement("span");
-  button.classList.add("glyphicon");
-  button.classList.add("glyphicon-remove-circle");
+    var button = document.createElement("span");
+    button.classList.add("glyphicon");
+    button.classList.add("glyphicon-remove-circle");
 
-  var inputValue = document.querySelector(".myInput").value;
-  var t = document.createTextNode(inputValue);
-  li.appendChild(t);
-  li.appendChild(button)
+    var inputValue = document.querySelector(".myInput").value;
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    li.appendChild(button)
 
-   if(e.keyCode == 13 && inputValue !=='') {
-   li.className = "newEl"
-   li.classList.add("list-group-item")
-   document.querySelector(".myUl").appendChild(li);
-   document.querySelector(".myInput").value = '';
-  }
+    if (e.keyCode == 13 && inputValue !== '') {
+        li.className = "newEl"
+        li.classList.add("list-group-item")
+        document.querySelector(".myUl").appendChild(li);
+        document.querySelector(".myInput").value = '';
+    }
 
-   var remove = document.querySelectorAll(".list-group-item");
-   var i;
-   for (i = 0; i < remove.length; i++) {
-    remove[i].onclick = function() {
-    var div = this.parentElement;
-    this.remove(div);
-  }
-}
+    var remove = document.querySelectorAll(".list-group-item");
+    var i;
+    for (i = 0; i < remove.length; i++) {
+        remove[i].onclick = function() {
+            var div = this.parentElement;
+            this.remove(div);
+        }
+    }
 
 
 });
-
 
 
 
